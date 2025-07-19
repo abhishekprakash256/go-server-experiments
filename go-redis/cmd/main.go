0c4600b024dff20921ac2322910b0406dcf3f731
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-07-19 15:03:21 -0400

    test code

--- go-redis/cmd/main.go
+++ go-redis/cmd/main.go
@@ -62,4 +62,40 @@ func main() {
 	} else {
 		log.Println("Session data deleted successfully")
 	}
+
+
+	// Prepare test session data
+	sessionKeyTwo := "session:abc123:Abhi"
+	sessiontwo := config.SessionData{
+		ChatID:      "abc123",
+		Sender:        "Anny",
+		Reciever: 		"Abhi",
+		LastSeen:    time.Now(),
+		WSConnected: 1,
+		Notify:      0,
+	}
+
+	// Store session data
+	ok = crud.StoreSessionData(ctx, client, sessionKeyTwo, sessiontwo)
+	if !ok {
+		log.Println("Failed to store session data")
+	} else {
+		log.Println("Session data stored successfully")
+	}
+
+	// Retrieve session data
+	retrieved, err = crud.GetSessionData(ctx, client, sessionKeyTwo)
+	if err != nil {
+		log.Println("Failed to fetch session data:", err)
+	} else {
+		fmt.Printf("Fetched session data: %+v\n", retrieved)
+	}
+
+	// Delete session data
+	deleted = crud.DeleteSessionData(ctx, client, sessionKeyTwo)
+	if !deleted {
+		log.Println("Failed to delete session data")
+	} else {
+		log.Println("Session data deleted successfully")
+	}
 }
