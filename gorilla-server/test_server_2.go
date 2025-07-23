cb779b8585fad8d907aa3773dbc652b741892685
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-07-22 22:23:17 -0400

    test code

--- gorilla-server/test_server_2.go
+++ gorilla-server/test_server_2.go
@@ -1,4 +1,43 @@
 /*
 this is a file to test the go gorilla server 
+
+
+https://tutorialedge.net/golang/go-websocket-tutorial/
+
+
 */
 
+package main 
+
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HomePage(w http.ResponseWriter , r *http.Request) {
+	 fmt.Fprintf(w, "Home Page")
+
+}
+
+func wsEndPoint(w http.ResponseWriter, r *http.Request ) {
+
+	fmt.Fprintf(w, "Hello World")
+}
+
+
+func setupRoutes() {
+
+	http.HandleFunc("/", HomePage)
+    http.HandleFunc("/ws", wsEndPoint)
+}
+
+func main() {
+
+	fmt.Println("In the main")
+
+	setupRoutes()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
